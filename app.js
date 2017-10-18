b647d301fd68a25185e1971669603c6ce6a43161
Roshni Kutty <roshni.ajkutty@gmail.com>
2017-10-18 11:52:15 -0400

    updated to scroll slower

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $('.name').click(function (event) {
     event.preventDefault();
-    $('body, html').animate({ scrollTop: 0 }, 'slow');
+    $('body, html').animate({ scrollTop: 0 }, 1500);
 });
 
 $('.js-more-button').click(function (event) {
@@ -8,12 +8,12 @@ $('.js-more-button').click(function (event) {
     $('#more').toggleClass('hidden');
     $('.js-more-button').addClass('hidden');
     $('.js-scroll-top').removeClass('hidden');
-    $('body, html').animate({ scrollTop: $(document).height() }, 'slow');
+    $('body, html').animate({ scrollTop: $(document).height() }, 1500);
 })
 
 $('.js-scroll-top').on('click', function (event) {
     event.preventDefault();
-    $('body, html').animate({ scrollTop: 0 }, 'slow');
+    $('body, html').animate({ scrollTop: 0 }, 1500);
     $('#more').addClass('hidden');
     $('.js-scroll-top').addClass('hidden');
     //Adding setTimeout so that the more button does not show before scroll up
